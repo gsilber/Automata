0eeefb12b64acc6a64c61daaa650f277f4fde1a8
Greg Silber <gsilber@cyberdaptive.com>
2018-09-22 16:42:05 -0400

    quick fix of syntax error

--- Automata/src/app/app-routing.module.ts
+++ Automata/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomePageComponent }
+  {path: 'home', component: HomePageComponent },
   {path: 'fsm', loadChildren: './modules/fsm/fsm.module#FsmModule'},
   {path: 'pda', loadChildren: './modules/pda/pda.module#PdaModule'},
   {path: 'turing', loadChildren: './modules/turing/turing.module#TuringModule'}
