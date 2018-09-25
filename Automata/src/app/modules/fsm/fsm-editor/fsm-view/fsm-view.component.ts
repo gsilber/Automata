00595be5fc33181c2dfab0c02a37780778c0ccf5
Greg Silber <gsilbercd@gmail.com>
2018-09-25 13:31:16 -0400

    setup for transitions

--- Automata/src/app/modules/fsm/fsm-editor/fsm-view/fsm-view.component.ts
+++ Automata/src/app/modules/fsm/fsm-editor/fsm-view/fsm-view.component.ts
@@ -4,101 +4,113 @@ import { FSM, FsmState, FsmTransition, StateMouseEvent, TransMouseEvent } from '
 
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'fsm-view',
-  templateUrl: './fsm-view.component.html',
-  styleUrls: ['./fsm-view.component.css']
+    // tslint:disable-next-line:component-selector
+    selector: 'fsm-view',
+    templateUrl: './fsm-view.component.html',
+    styleUrls: ['./fsm-view.component.css']
 })
 export class FsmViewComponent implements OnInit {
-  private _scale = 1;
-  @ViewChild('outsideDiv') outsideDiv: ElementRef;
+    private _scale = 1;
+    @ViewChild('outsideDiv') outsideDiv: ElementRef;
 
-  // State Mouse Events
-  @Output() stateClick: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateDblClick: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseDown: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseUp: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseEnter: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseLeave: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseMove: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
-  @Output() stateMouseOver: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    // State Mouse Events
+    @Output() stateClick: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateDblClick: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseDown: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseUp: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseEnter: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseLeave: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseMove: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
+    @Output() stateMouseOver: EventEmitter<StateMouseEvent> = new EventEmitter<StateMouseEvent>();
 
-  // Trans Mouse Events
-  @Output() transClick: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transDblClick: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseDown: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseUp: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseEnter: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseLeave: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseMove: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
-  @Output() transMouseOver: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    // Trans Mouse Events
+    @Output() transClick: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transDblClick: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseDown: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseUp: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseEnter: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseLeave: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseMove: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
+    @Output() transMouseOver: EventEmitter<TransMouseEvent> = new EventEmitter<TransMouseEvent>();
 
-  // Scaling and scrolling values
-  @Input() canvasSize = 4000;
-  @Input() fsm: FSM;
-  @Input() stateRadius = 25;
-  @Input() inEditor = false;
-  @Input() selected: Object = null;
+    // Scaling and scrolling values
+    @Input() canvasSize = 4000;
+    @Input() fsm: FSM;
+    @Input() stateRadius = 25;
+    @Input() inEditor = false;
+    @Input() selected: Object = null;
 
-  set scalePercent(percent: number) {
-    const val = percent / 100;
-    if (val <= 2 && val >= .25) {
-      this._scale = val;
+    set scalePercent(percent: number) {
+        const val = percent / 100;
+        if (val <= 2 && val >= .25) {
+            this._scale = val;
+        }
     }
-  }
-  get scalePercent(): number {
-    return Math.floor(this._scale * 100);
-  }
-  get windowSize(): number { return this.canvasSize * 4; }
-  get scrollSize(): number { return this.canvasSize * this._scale; }
-  get scaleBox(): string {
-    return '0 0 ' + (this.windowSize / this._scale) + ' ' + (this.windowSize / this._scale);
-  }
-
-  // application
-  constructor(_fsmSvc: FsmService) {
-    this.fsm = _fsmSvc.getFsm();
-  }
-
-  ngOnInit() {
-  }
-
-  // encorce window is a square
-  @HostListener('window:resize') onResize() {
-    if (this.outsideDiv) {
-      this.outsideDiv.nativeElement.height = this.outsideDiv.nativeElement.clientWidth;
+    get scalePercent(): number {
+        return Math.floor(this._scale * 100);
     }
-  }
-
-  // State Event Handlers
-  onStateClick(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateClick, state, event); }
-  onStateDblClick(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateDblClick, state, event); }
-  onStateMouseDown(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseDown, state, event); }
-  onStateMouseUp(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseUp, state, event); }
-  onStateMouseEnter(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseEnter, state, event); }
-  onStateMouseLeave(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseLeave, state, event); }
-  onStateMouseMove(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseMove, state, event); }
-  onStateMouseOver(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseOver, state, event); }
-
-  emitStateMouseEvent(destEvent: EventEmitter<StateMouseEvent>, state: FsmState, srcEvent: MouseEvent) {
-    if (this.inEditor) {
-      destEvent.emit(new StateMouseEvent(state, srcEvent));
+    get windowSize(): number { return this.canvasSize * 4; }
+    get scrollSize(): number { return this.canvasSize * this._scale; }
+    get scaleBox(): string {
+        return '0 0 ' + (this.windowSize / this._scale) + ' ' + (this.windowSize / this._scale);
     }
-  }
-  // Transition EventHandlers
-  onTransClick(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transClick, trans, event); }
-  onTransDblClick(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transDblClick, trans, event); }
-  onTransMouseDown(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseDown, trans, event); }
-  onTransMouseUp(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseUp, trans, event); }
-  onTransMouseEnter(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseEnter, trans, event); }
-  onTransMouseLeave(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseLeave, trans, event); }
-  onTransMouseMove(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseMove, trans, event); }
-  onTransMouseOver(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseOver, trans, event); }
 
-  emitTransMouseEvent(destEvent: EventEmitter<TransMouseEvent>, trans: FsmTransition, srcEvent: MouseEvent) {
-    if (this.inEditor) {
-      destEvent.emit(new TransMouseEvent(trans, srcEvent));
+    // application
+    constructor(_fsmSvc: FsmService) {
+        this.fsm = _fsmSvc.getFsm();
     }
-  }
+
+    ngOnInit() {
+    }
+
+    getTransPath(state: FsmState, trans: FsmTransition): string {
+        // get the distance between the points state and trans.dest
+        // return a path along x axis of that length from state with a fixed vertical offset and midpoint
+        // <path [attr.d]='"M 100 350 q 150 -300 300 0"' stroke="blue"
+
+        return '';
+    }
+    getTransTrasform(state: FsmState, trans FsmTransition): string {
+        // calculate the angle and rotate around the center point of state
+        return '';
+    }
+
+    // encorce window is a square
+    @HostListener('window:resize') onResize() {
+        if (this.outsideDiv) {
+            this.outsideDiv.nativeElement.height = this.outsideDiv.nativeElement.clientWidth;
+        }
+    }
+
+    // State Event Handlers
+    onStateClick(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateClick, state, event); }
+    onStateDblClick(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateDblClick, state, event); }
+    onStateMouseDown(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseDown, state, event); }
+    onStateMouseUp(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseUp, state, event); }
+    onStateMouseEnter(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseEnter, state, event); }
+    onStateMouseLeave(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseLeave, state, event); }
+    onStateMouseMove(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseMove, state, event); }
+    onStateMouseOver(event: MouseEvent, state: FsmState) { this.emitStateMouseEvent(this.stateMouseOver, state, event); }
+
+    emitStateMouseEvent(destEvent: EventEmitter<StateMouseEvent>, state: FsmState, srcEvent: MouseEvent) {
+        if (this.inEditor) {
+            destEvent.emit(new StateMouseEvent(state, srcEvent));
+        }
+    }
+    // Transition EventHandlers
+    onTransClick(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transClick, trans, event); }
+    onTransDblClick(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transDblClick, trans, event); }
+    onTransMouseDown(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseDown, trans, event); }
+    onTransMouseUp(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseUp, trans, event); }
+    onTransMouseEnter(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseEnter, trans, event); }
+    onTransMouseLeave(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseLeave, trans, event); }
+    onTransMouseMove(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseMove, trans, event); }
+    onTransMouseOver(event: MouseEvent, trans: FsmTransition) { this.emitTransMouseEvent(this.transMouseOver, trans, event); }
+
+    emitTransMouseEvent(destEvent: EventEmitter<TransMouseEvent>, trans: FsmTransition, srcEvent: MouseEvent) {
+        if (this.inEditor) {
+            destEvent.emit(new TransMouseEvent(trans, srcEvent));
+        }
+    }
 
 }
